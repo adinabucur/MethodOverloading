6cc7a3320e724ce40a12c497e2df3643508be061
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-07-15 20:57:01 +0300

    Update
    Introducing constants

--- src/com/learnprogramming/SecondsAndMinutesChallenge.java
+++ src/com/learnprogramming/SecondsAndMinutesChallenge.java
@@ -1,31 +1,47 @@
 package com.learnprogramming;
 
 public class SecondsAndMinutesChallenge {
+
+    public static final String INVALID_VALUE_MESSAGE = "Invalid value.";
+
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
-
-
-        String secondResult = getDurationString(3945);
-        System.out.println(secondResult);
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
+        
     }
 
     public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         } else if (seconds < 0 || seconds > 59) {
             return "Invalid Value";
         }
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
 
+        String hoursString = hours + "h";
+        if (hours < 10) {
+            hoursString = "0" + hoursString;
+        }
 
-        return hours + "h " + remainingMinutes + "m " +
-                seconds + "s ";
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minutesString + " " + secondsString + " ";
     }
 
     public static String getDurationString(int seconds) {
         if (seconds < 0) {
-            return "Invalid Value.";
+            return INVALID_VALUE_MESSAGE;
         }
         int minutes = seconds / 60;
         int remainingSeconds = seconds % 60;
