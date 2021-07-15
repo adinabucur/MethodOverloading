6620b67da949e8ac29299a288c37891885ab9355
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-07-15 17:05:24 +0300

    Update
    Challenge

--- src/com/learnprogramming/Exercise.java
+++ src/com/learnprogramming/Exercise.java
@@ -2,6 +2,8 @@ package com.learnprogramming;
 
 public class Exercise {
     public static void main(String[] args) {
+
+
         double result = calcFeetAndInchesToCentimeters(2, 2);
         System.out.println(result);
 
@@ -11,12 +13,16 @@ public class Exercise {
     }
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
-        if (feet < 0 || (inches < 0 && inches > 12)) {
+        if (feet < 0 || (inches < 0 || inches > 12)) {
+            System.out.println("Invalid feet or inches parameters.");
             return -1;
         }
         double feetToCentimeters = feet * 12 * 2.54;
         double inchesToCentimeters = inches * 2.54;
-        return feetToCentimeters + inchesToCentimeters;
+        double centimeters = feetToCentimeters + inchesToCentimeters;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters);
+        return centimeters;
+
     }
 
     public static double calcFeetAndInchesToCentimeters(double inches) {
